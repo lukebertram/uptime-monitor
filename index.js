dfa9106c7359090d2b13680ceadab132d881edf7
Luke Bertram <lukebertram@gmail.com>
2018-05-30 21:51:11 -0700

    set content-type header of api responses to application/json

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ const server = http.createServer((req, res) => {
       const payloadString = JSON.stringify(payload);
 
       // Return the response
+      res.setHeader('Content-Type', 'application/json');
       res.writeHead(statusCode);
       res.end(payloadString);
       console.log('Returning this response: ', statusCode, payloadString);
