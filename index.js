ea42069f134c9e33d48de457fc89bd302aa2fa5a
Luke Bertram <lukebertram@gmail.com>
2018-06-10 18:09:28 -0700

    add POST and GET actions to checks/ endpoint

--- index.js
+++ index.js
@@ -103,5 +103,6 @@ const unifiedServer = (req, res) => {
 const router = {
   'ping' : handlers.ping,
   'users': handlers.users,
-  'tokens': handlers.tokens
+  'tokens': handlers.tokens,
+  'checks': handlers.checks
 };
